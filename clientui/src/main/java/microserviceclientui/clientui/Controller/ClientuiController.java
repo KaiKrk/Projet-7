b2dadadc7be149c913e6af2cf9bab8df6edd87e1
KaiKrk <kaiqiang@outlook.fr>
2020-03-10 23:31:58 +0100

    Spring Security, Jwt transfered to Zuul Module

--- clientui/src/main/java/microserviceclientui/clientui/Controller/ClientuiController.java
+++ clientui/src/main/java/microserviceclientui/clientui/Controller/ClientuiController.java
@@ -12,18 +12,12 @@ import org.springframework.http.ResponseEntity;
 //import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 
 @RestController
 public class ClientuiController {
 
-//    @Autowired
-//    private JwtUtil jwtTokenUtil;
-//
-//    @Autowired
-//    private AuthenticationManager authenticationManager;
-//
-//    @Autowired
-//    private MyUserDetailsService userDetailsService;
 
     @Autowired
     MicroserviceBookProxy microserviceBookProxy;
@@ -35,31 +29,11 @@ public class ClientuiController {
 
     @GetMapping("books")
     public String books(){
-//        List<BookBean> books = microserviceBookProxy.books();
+        List<BookBean> books = microserviceBookProxy.books();
         return "books";
     }
 
 
-//    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
-//    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
-//
-//        try {
-//            authenticationManager.authenticate(
-//                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
-//            );
-//        }
-//        catch (BadCredentialsException e) {
-//            throw new Exception("Incorrect username or password", e);
-//        }
-//
-//
-//        final UserDetails userDetails = userDetailsService
-//                .loadUserByUsername(authenticationRequest.getUsername());
-//
-//        final String jwt = jwtTokenUtil.generateToken(userDetails);
-//
-//        return ResponseEntity.ok(new AuthenticationResponse(jwt));
-//    }
 
 }
 
